1c3775b99954ce568ec46bb9266c27be505a1f50
@kidpollo <kidpollo@gmail.com>
2010-10-22 17:59:24 -0500

    small initializer fix

--- lib/tanker/railtie.rb
+++ lib/tanker/railtie.rb
@@ -3,14 +3,21 @@ require 'rails'
 module Tanker
   class Railtie < Rails::Railtie
     config.index_tank_url = ''
-
+    
+    initializer "tanker.boot" do
+      get_config
+    end
+    
     config.after_initialize do
-      Tanker.configuration = {:url => config.index_tank_url }
+      get_config
     end
 
     rake_tasks do
       load "tanker/tasks/tanker.rake"
     end
-
+    
+    def get_config
+      Tanker.configuration = {:url => config.index_tank_url }
+    end
   end
 end
