63feeeac08890621207e00494603d46b3983efce
@kidpollo <kidpollo@gmail.com>
2010-10-22 18:30:28 -0500

    small fix again

--- lib/tanker/railtie.rb
+++ lib/tanker/railtie.rb
@@ -5,19 +5,15 @@ module Tanker
     config.index_tank_url = ''
     
     initializer "tanker.boot" do
-      get_config
+      Tanker.configuration = {:url => config.index_tank_url }
     end
     
     config.after_initialize do
-      get_config
+      Tanker.configuration = {:url => config.index_tank_url }
     end
 
     rake_tasks do
       load "tanker/tasks/tanker.rake"
-    end
-    
-    def get_config
-      Tanker.configuration = {:url => config.index_tank_url }
     end
   end
 end
